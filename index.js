0f58e22aba19d3c0816bf520e2d1650bb72b4d83
Mikazakaryan <mikazakaryan94@gmail.com>
2020-07-16 17:01:01 +0400

    conditions and loops examples

---
+++ index.js
@@ -0,0 +1,122 @@
+// const firstNumber = prompt("Please insert a number");
+// const secondNumber = prompt("Please insert a number");
+
+// if (parseInt(firstNumber) <= parseInt(secondNumber)) {
+//   alert(`first is smaller or equal ${firstNumber} ${secondNumber}`);
+// } else {
+//   alert(`first is bigger ${firstNumber} ${secondNumber}`);
+// }
+
+// alert(4 == "4"); // true
+// alert(4 === "4"); // false
+
+// const number = parseInt(prompt("Please insert a number"));
+
+// switch (number) {
+//   case 1:
+//     alert("1");
+//     break;
+//   case 2:
+//     alert("2");
+//     break;
+//   case 3:
+//     alert("3");
+//     break;
+//   default:
+//     alert("default");
+//     break;
+// }
+
+// const first = 4;
+// const second = 19;
+
+// if (first !== second) {
+//   alert(`both are true`);
+// }
+
+// const check = false;
+
+// const variable = check ? 5 : 19;
+
+// let variable;
+
+// if (check) {
+//   variable = 5;
+// } else {
+//   variable = 19;
+// }
+
+// alert(variable);
+
+// if (check) {
+//   alert("true");
+// } else {
+//   alert("asga");
+//   alert("false");
+// }
+
+// for (let index = 0; index <= 10; ++index) {
+//   console.log("1111", index);
+// }
+
+// let check = false;
+
+// while (check) {
+//   console.log("111111");
+//   check = false;
+// }
+
+// do {
+//   console.log("111111");
+// } while (check);
+
+// const students = [
+//   {
+//     grade: 2,
+//   },
+//   {
+//     grade: 3,
+//   },
+//   {
+//     grade: 7,
+//   },
+//   {
+//     grade: 19,
+//   },
+//   {
+//     grade: 5,
+//   },
+//   {
+//     grade: 25,
+//   },
+//   {
+//     grade: 51,
+//   },
+//   {
+//     grade: 53,
+//   },
+//   {
+//     grade: 35,
+//   },
+//   {
+//     grade: 95,
+//   },
+// ];
+
+// const matric = [
+//   ["*", "*", "*", "*", "*"],
+//   ["*", "*", "*", "*", "*"],
+//   ["*", "*", "*", "*", "*"],
+//   ["*", "*", "*", "*", "*"],
+//   ["*", "*", "*", "*", "*"],
+// ];
+
+// for (let i = 0; i < matric.length; ++i) {
+//   for (let j = 0; j < matric[i].length; j++) {
+//     document.write(`${matric[i][j]}   `);
+//   }
+//   document.write("\n");
+// }
+
+const number = prompt("fs");
+alert(1 + Number(number));
